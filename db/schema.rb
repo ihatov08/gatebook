3ed778f10797fb03ec8b3f348fd291418c696ffb
tomoya <ihatov08@gmail.com>
2016-02-20 17:55:59 +0900

    Userモデルにログイン機能をつけよう

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160220053157) do
+ActiveRecord::Schema.define(version: 20160220084233) do
 
   create_table "notes", force: :cascade do |t|
     t.string   "title"
@@ -24,9 +24,10 @@ ActiveRecord::Schema.define(version: 20160220053157) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.string   "image"
+    t.string   "encrypted_password", default: "", null: false
   end
 
 end
