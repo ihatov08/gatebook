93b6975e7c34106befe732336d10e1139c752bdc
tomoya <ihatov08@gmail.com>
2016-02-20 15:24:09 +0900

    プロフィール画像を表示しよう

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,11 @@
 module UsersHelper
+  def image_for(user)
+    if user.image
+      # image_tagを用いてuserのプロフィール画像を表示してください
+      image_tag "/user_images/#{user.image}", class: "profile_img"
+    else
+      # image_tagを用いてwanko.pngを表示してください
+      image_tag "twitterデフォルトアイコン.png", class: "profile_img"
+    end
+  end
 end
