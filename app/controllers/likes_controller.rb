3c575f04080a72f8da9e8eefa93fb05953d57597
tomoya <ihatov08@gmail.com>
2016-02-21 11:23:17 +0900

    いいね！機能を実装しよう

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,5 +1,11 @@
 class LikesController < ApplicationController
   def like
+    note = Note.find(params[:note_id])
+    # 変数likeに、current_userとbuildを用いてLikeインスタンスを代入してください
+    like = current_user.likes.build(note_id: note.id)
+    # saveメソッドで、likeを保存してください
+    like.save
+    redirect_to note
   end
   def unlike
   end
